a1ead7d83f614425b41052f28e50446d74710c69
Myers <meyennie@gmail.com>
2016-11-05 12:25:08 -0500

    4.0

--- lagos_game.py
+++ lagos_game.py
@@ -4,7 +4,8 @@ from sys import exit
 
 def start():
 
-	print """You just arrived in Lagos Nigeria and asked to be dropped off based on a number you choose between 0 to 10, what's your pick?"""
+	print "You just arrived in Lagos Nigeria and asked to be dropped off" 
+	print "based on a number you choose between 0 to 10, what's your pick?"
 	
 	valid_input = False
 	while not valid_input:
@@ -35,9 +36,9 @@ def start():
 
 def ajegunle():
 
-	print """Low expectation, low results. You are dropped off in Ajegunle. 
-This is the rough part of Lagos %s, and you definitly need to stay safe, 
-what do you pick for protection? juju, a gun or wisdom?""" % name
+	print "Low expectation, low results. You are dropped off in Ajegunle." 
+	print "This is the rough part of Lagos %s, and you definitly need to stay safe," 
+	print "what do you pick for protection? juju, a gun or wisdom?" % name
 
 	valid_input = False
 	while not valid_input:
@@ -50,14 +51,14 @@ what do you pick for protection? juju, a gun or wisdom?""" % name
 
 		if "juju" in protection:
 
-			print """Very smart idea to pick something you don't know how to use.
-You light yourself on fire and burn to ashes."""
+			print "Very smart idea to pick something you don't know how to use."
+			print "You light yourself on fire and burn to ashes."
 			exit(0)
 
 		elif "gun" in protection:
 
-			print """Have you ever used a gun before? You accidentally shoot yourself
-because you don't know how to use one. You're eaten alive by fire ants"""
+			print "Have you ever used a gun before? You accidentally shoot yourself"
+			print "because you don't know how to use one. You're eaten alive by fire ants"
 			exit(0)
 
 		elif "wisdom" in protection:
@@ -75,11 +76,11 @@ because you don't know how to use one. You're eaten alive by fire ants"""
 
 def wisdom():
 
-	print """Good choice, you would need to be street smart to survive around here.
-Let's see how you use this wisdom. So you get stuck at 2am in a very sketchy place 
-and you have no money. The only was back is a bus that would cost N500, it's your 
-lucky day and you see a N500 note crumbled below you. What do you do? 
-Pick it up, ignore it or kill the driver and steal the bus?"""
+	print "Good choice, you would need to be street smart to survive around here."
+	print "Let's see how you use this wisdom. So you get stuck at 2am in a very sketchy place" 
+	print "and you have no money. The only was back is a bus that would cost N500, it's your" 
+	print "lucky day and you see a N500 note crumbled below you. What do you do?"
+	print "Pick it up, ignore it or kill the driver and steal the bus?"
 
 	valid_input = False
 	while not valid_input:
@@ -92,14 +93,16 @@ Pick it up, ignore it or kill the driver and steal the bus?"""
 
 		if "pick it up" in bus:
 
-			print """Bad idea to pick up items you don't own here.
-You turn into yam and you are used to make yam porridge to feed your neighbours dog."""
+			print "Bad idea to pick up items you don't own here."
+			print "You turn into yam and you are used to make yam porridge"
+			print "to feed your neighbours dog."
 			exit(0)
 
 		elif "ignore" in bus:
 
-			print """You get kidnapped and because friends and family don't like you, you get sold for
-$0 to a cannibal who slowly cuts you to pieces and eats you for breakfast every day."""
+			print "You get kidnapped and because friends and family don't like you," 
+			print "you get sold for $0 to a cannibal who slowly cuts you to pieces" 
+			print "and eats you for breakfast every day."
 			exit(0)
 
 		elif "steal" in bus:
@@ -117,9 +120,9 @@ $0 to a cannibal who slowly cuts you to pieces and eats you for breakfast every 
 
 def ikoyi():
 
-	print """High expectation, high results %s. You are dropped off in 
-Ikoyi, good place to start off and lots of things to do. 
-What would you like to do? Tour town or go relax in ikoyi club?""" % name
+	print "High expectation, high results %s. You are dropped off in"
+	print "Ikoyi, good place to start off and lots of things to do." 
+	print "What would you like to do? Tour town or go relax in ikoyi club?" % name
 
 	valid_input = False
 	while not valid_input:
@@ -150,8 +153,9 @@ What would you like to do? Tour town or go relax in ikoyi club?""" % name
 
 def hawker():
 
-	print """During the tour, you are thirsty and want to buy a drink from a street hawker.
-You ask for water and he says it cost N3,000. What do you do? drive off or buy it?"""
+	print "During the tour, you are thirsty and want to buy a drink"
+	print "from a street hawker. You ask for water and he says it cost N3,000."
+	print "What do you do? drive off or buy it?"
 	
 	valid_input = False
 	while not valid_input:
@@ -169,8 +173,8 @@ You ask for water and he says it cost N3,000. What do you do? drive off or buy i
 
 		elif "buy it" in thirst:
 
-			print """You clearly are not a smart %r. Who buys water for N3,000?.
-You wouldn't last in Lagos.""" % name
+			print "You clearly are not a smart %r. Who buys water for N5,000?.
+			print "You wouldn't last in Lagos." % name
 			exit(0)
 
 		else:
@@ -183,9 +187,9 @@ You wouldn't last in Lagos.""" % name
 
 def email():
 
-	print """You definitly need the relaxation. While doing so, you decide to check your email
-and a Prince in Nigeria notifies you that he needs money to cover the cost of his mother's funeral.
-What do you do? Send money or ignore?"""
+	print "You definitly need the relaxation. While doing so, you decide to check"
+	print "your email and a Prince in Nigeria notifies you that he needs money to"
+	print "cover the cost of his mother's funeral. What do you do? Send money or ignore?"
 
 	valid_input = False
 	while not valid_input:
@@ -198,13 +202,13 @@ What do you do? Send money or ignore?"""
 
 		if "money" in mother:
 	
-			print """You get scammed. You're obviously broke and lose your life savings, 
-$632.51 to him."""
+			print "You get scammed. You're obviously broke and" 
+			print "lose your life savings, $63.51 to him."
 			exit(0)
 
 		elif "ignore" in mother:
 		
-			print """You're heartless. Society doesn't need people like you."""
+			print "You're heartless. Society doesn't need people like you."
 			exit(0)
 
 		else:
