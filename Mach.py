bf4cc8b725370b08fee548f8b199a293415dd471
SirMyers <meyennie@gmail.com>
2016-12-17 19:45:19 -0600

    Delete Mach.py

--- Mach.py
+++
@@ -1,6 +0,0 @@
-def manchesterunited (david, ryan):
-	print "%s scored the goal from the half way line" %david
-	print "while %s, scored that badass goal against arsenal" %ryan
-
-print "what the fuck"
-manchesterunited("ba", "cha")
