e537ed07dbb3ed052e3a0670ba78b3ee3f02a89f
Myers <meyennie@gmail.com>
2017-01-01 14:28:38 -0600

    3.0

---
+++ template.py
@@ -0,0 +1,137 @@
+from sys import exit
+from random import randint
+#ex 43
+
+##################
+
+def abc():
+
+    print "abc"
+    print "abc"
+
+    valid_input = False
+    while not valid_input:
+
+        print ""
+######
+        abc = int(raw_input(">"))
+######
+        print ""
+
+        if abc >= 0 and abc < 0:
+
+            abc()
+            valid_input = True
+
+        elif abc >= 0 and abc < 0:
+
+            abc()
+            valid_input = True
+
+        else:
+
+            print "abc"
+
+        print ""
+
+##################
+
+def abc():
+
+    print "abc"
+    print "abc"
+
+    valid_input = False
+    while not valid_input:
+
+        print ""
+######
+        abc = raw_input(">")
+######
+        print ""
+
+        if "abc" in abc:
+
+            print "abc"
+            print "abc"
+            exit(0)
+
+        elif "abc" in abc:
+
+            print "abc"
+            print "abc"
+            exit(0)
+
+        elif "abc" in abc:
+
+            abc()
+            valid_input = True
+
+        else:
+
+            print "abc"
+
+        print ""
+
+#################
+
+def abc():
+
+    print "abc"
+    print "abc"
+
+    valid_input = False
+    while not valid_input:
+
+        print ""
+######
+        abc = raw_input(">")
+######
+        print ""
+        
+        #code = "%d%d%d" % (randint(1,9), randint(1,9), randint(1,9))
+        #guess = raw_input("[keypad]> ")
+        #guesses = 0
+
+        #while guess != code and guesses < 10:
+            #print "BZZZZEDDD!"
+            #guesses += 1
+            #guess = raw_input("[keypad]> ")
+
+        if "abc" in abc:
+        #if guess == code:
+            print "abc"
+            print "abc"
+            exit(0)
+
+        elif "abc" in abc:
+
+            print "abc"
+            print "abc"
+            exit(0)
+
+        elif "abc" in abc:
+
+            abc()
+            valid_input = True
+
+        else:
+
+            print "abc"
+
+        print ""
+
+#######################################################################
+
+print "abc"
+
+print ""
+######
+abc = raw_input(">")
+######
+print ""
+
+print "abc"
+
+print ""
+start() 
